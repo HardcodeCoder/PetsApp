3199e68b37eb3e06a899c484cf4da76a50ec0451
Ashutosh <ashutoshpatoa3105@gmail.com>
2019-04-01 18:36:39 +0530

    Initial commit

---
+++ app/src/main/java/com/hardcodecoder/petsapp/data/PetDbHelper.java
@@ -0,0 +1,54 @@
+package com.hardcodecoder.petsapp.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.hardcodecoder.petsapp.data.PetContract.PetEntry;
+
+
+public class PetDbHelper extends SQLiteOpenHelper {
+
+    private static final String LOG_TAG = PetDbHelper.class.getSimpleName();
+
+    /**
+     * Name of the database file
+     */
+    private static final String DATABASE_NAME = "shelter.db";
+
+    /**
+     * Database version. If you change the database schema, you must increment the database version.
+     */
+    private static final int DATABASE_VERSION = 1;
+
+    /**
+     * Constructs a new instance of {@link PetDbHelper}.
+     *
+     * @param context of the app
+     */
+    public PetDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    /**
+     * This is called when the database is created for the first time.
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
+                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
+                + PetEntry.COLUMN_PET_BREED + " TEXT, "
+                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
+                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+        Log.e(LOG_TAG, SQL_CREATE_PETS_TABLE);
+        db.execSQL(SQL_CREATE_PETS_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}

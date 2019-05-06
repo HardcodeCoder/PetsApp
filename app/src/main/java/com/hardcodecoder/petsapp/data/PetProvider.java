3c25bd28e0f1ea9713496bffa1865e69f0d70f83
Ashutosh <ashutoshpatoa3105@gmail.com>
2019-05-06 18:31:37 +0530

    added UriMatcher to check whether query uri is valid

--- app/src/main/java/com/hardcodecoder/petsapp/data/PetProvider.java
+++ app/src/main/java/com/hardcodecoder/petsapp/data/PetProvider.java
@@ -1,17 +1,43 @@
 package com.hardcodecoder.petsapp.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import com.hardcodecoder.petsapp.data.PetContract.PetEntry;
 
 public class PetProvider extends ContentProvider {
 
+    /** URI matcher code for the content URI for the pets table */
+    private static final int PETS = 100;
+
+    /** URI matcher code for the content URI for a single pet in the pets table */
+    private static final int PET_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // The calls to addURI() go here, for all of the content URI patterns that the provider
+        // should recognize. All paths added to the UriMatcher have a corresponding code to return
+        // when a match is found.
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS);
+        sUriMatcher.addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS + "/#", PET_ID);
+    }
+
     private PetDbHelper mDbHelper;
-    private static final String TAG = PetProvider.class.getSimpleName();
+    // private static final String TAG = PetProvider.class.getSimpleName();
 
     @Override
     public boolean onCreate() {
@@ -22,8 +48,29 @@ public class PetProvider extends ContentProvider {
     @Nullable
     @Override
     public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
+        SQLiteDatabase db = mDbHelper.getReadableDatabase();
+
+        //Cursor that will hold the result of the query
+        Cursor c;
+
+        int match = sUriMatcher.match(uri);
+        switch(match){
+            case PETS:
+                c = db.query(PetEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            case PET_ID:
+                selection = PetEntry._ID + "=?";
+
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                c = db.query(PetEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return c;
     }
+
 
     @Nullable
     @Override

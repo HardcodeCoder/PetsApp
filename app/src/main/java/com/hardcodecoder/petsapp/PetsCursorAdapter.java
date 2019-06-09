9e14398f6320b58fb659c37b63c504d1a39a92f4
Ashutosh <ashutoshpatoa3105@gmail.com>
2019-06-09 18:19:06 +0530

    Initial commit

---
+++ app/src/main/java/com/hardcodecoder/petsapp/PetsCursorAdapter.java
@@ -0,0 +1,32 @@
+package com.hardcodecoder.petsapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.hardcodecoder.petsapp.data.PetContract.PetEntry;
+
+public class PetsCursorAdapter extends CursorAdapter {
+
+
+    public PetsCursorAdapter(Context context, Cursor c){
+        super(context,c,0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.pets_list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView tv = view.findViewById(R.id.name);
+        tv.setText(cursor.getString(cursor.getColumnIndexOrThrow(PetEntry.COLUMN_PET_NAME)));
+        tv = view.findViewById(R.id.summary);
+        tv.setText(cursor.getString(cursor.getColumnIndexOrThrow(PetEntry.COLUMN_PET_BREED)));
+    }
+}
